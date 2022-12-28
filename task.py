ec458fda415ac56938a2599216129c3f7cf545d9
YounesOudah <younes.a.oudah@gmail.com>
2022-12-28 20:16:51 +0200

    task 1

---
+++ task.py
@@ -0,0 +1,7 @@
+message = ("Hello Mr\Ms")
+name = str (input("Enter your name"))
+age = int (input("Enter your age"))
+
+address = str (input("Enter your address"))
+message2 = ("thanks for beening one of our community,\t\t Enjoy")
+print(message, name, age, address, message2)
