ba4fbd2b499adc96b0bbd4ac32f6a2e3909ba809
Younes_Oudah <105824175+YounesOudah@users.noreply.github.com>
2023-01-02 13:25:11 +0200

    Update assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -1,11 +1,12 @@
-data = [12, 75, 150, 180, 145, 525, 50]
+numbers = [12, 75, 150, 180, 145, 525, 50]
 
 
-for value in data:
+for value in numbers:
     if value % 5 == 0:
-        continue
-        if value > 150:
+        if value <= 150:
+            print(value)
             continue
-        if value > 500:
-            break
-print(data)
+            if value >= 500:
+                        break
+print(numbers)
+        
